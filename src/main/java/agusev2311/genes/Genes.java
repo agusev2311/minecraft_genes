467b38b97e270680111dc844f030c9be0c41ca51
Artem Gusev <agusev2311@mail.ru>
2024-08-20 13:12:10 +0300

    Randomize exchange when pressing shift

--- src/main/java/agusev2311/genes/Genes.java
+++ src/main/java/agusev2311/genes/Genes.java
@@ -1,13 +1,33 @@
 package agusev2311.genes;
 
+import org.bukkit.attribute.Attribute;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.jetbrains.annotations.NotNull;
+import java.util.Random;
 
-public final class Genes extends JavaPlugin {
+import java.nio.file.Path;
+
+public final class Genes extends JavaPlugin implements @NotNull Listener {
 
     @Override
     public void onEnable() {
-        // Plugin startup logic
+        getServer().getPluginManager().registerEvents(this, this);
+    }
 
+    @EventHandler
+    public void onPlayerSneak(PlayerToggleSneakEvent event) {
+        Random rand = new Random();
+        double scale = (rand.nextInt(20) + 1) / 10.0;
+        event.getPlayer().getAttribute(Attribute.GENERIC_SCALE).setBaseValue(scale);
+        event.getPlayer().sendMessage("Now your size is " + scale * 1.9);
+    }
+
+    @Override
+    public @NotNull Path getDataPath() {
+        return super.getDataPath();
     }
 
     @Override
