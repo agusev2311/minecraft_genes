8b8f4c3970203bf42ebc2ce429a7566d91d978a1
Artem Gusev <agusev2311@mail.ru>
2024-08-20 14:14:09 +0300

    Added more attributes and more
    - Added more attributes and more
    - The function now reacts to the death of the player
    - Added an empty function that reacts to a player joining the server. In the future, it will do the same as the previous function.

--- src/main/java/agusev2311/genes/Genes.java
+++ src/main/java/agusev2311/genes/Genes.java
@@ -3,6 +3,8 @@ package agusev2311.genes;
 import org.bukkit.attribute.Attribute;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
+import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerToggleSneakEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.jetbrains.annotations.NotNull;
@@ -18,11 +20,20 @@ public final class Genes extends JavaPlugin implements @NotNull Listener {
     }
 
     @EventHandler
-    public void onPlayerSneak(PlayerToggleSneakEvent event) {
+    public void onPlayerDeath(PlayerDeathEvent event) {
         Random rand = new Random();
-        double scale = (rand.nextInt(20) + 1) / 10.0;
+        double scale = (rand.nextInt(15) + 1) / 10.0;
         event.getPlayer().getAttribute(Attribute.GENERIC_SCALE).setBaseValue(scale);
-        event.getPlayer().sendMessage("Now your size is " + scale * 1.9);
+//        event.getPlayer().sendMessage("Now your size is " + scale * 1.9);
+        event.getPlayer().getAttribute(Attribute.GENERIC_ARMOR).setBaseValue(rand.nextInt(3) + 1);
+        event.getPlayer().getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).setBaseValue((rand.nextInt(20) + 1) / 10.0);
+        event.getPlayer().getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(rand.nextInt(8) + 1);
+        event.getPlayer().getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(rand.nextInt(30) + 1);
+    }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+
     }
 
     @Override
