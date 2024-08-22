21317157af3dbe1107f9602070f512fad021ae7f
Artem Gusev <agusev2311@mail.ru>
2024-08-22 14:29:31 +0300

    Added mutation variables and more
    - mutation variables
    - edit max attack damage (2 -> 4)

--- src/main/java/agusev2311/genes/Genes.java
+++ src/main/java/agusev2311/genes/Genes.java
@@ -35,13 +35,29 @@ public final class Genes extends JavaPlugin implements @NotNull Listener {
         event.getPlayer().getAttribute(Attribute.GENERIC_JUMP_STRENGTH).setBaseValue(scale * 0.42); // 0.42
         event.getPlayer().getAttribute(Attribute.GENERIC_MOVEMENT_SPEED).setBaseValue(scale * 0.1); // 0.1
         double armor = rand.nextInt(3) + 1;
-        double attack_damage = (rand.nextInt(20) + 1) / 10.0;
+        double attack_damage = (rand.nextInt(40) + 1) / 10.0;
         double attack_speed = rand.nextInt(8) + 1;
         double max_health = rand.nextInt(30) + 1;
         event.getPlayer().getAttribute(Attribute.GENERIC_ARMOR).setBaseValue(armor); // 0
         event.getPlayer().getAttribute(Attribute.GENERIC_ATTACK_DAMAGE).setBaseValue(attack_damage); // 1
         event.getPlayer().getAttribute(Attribute.GENERIC_ATTACK_SPEED).setBaseValue(attack_speed); // 4
         event.getPlayer().getAttribute(Attribute.GENERIC_MAX_HEALTH).setBaseValue(max_health); // 20
+
+        int mutation_scale = rand.nextInt(5);
+        int mutation_jump_strength = rand.nextInt(5);
+        int mutation_movement_speed = rand.nextInt(5);
+        int mutation_armor = rand.nextInt(5);
+        int mutation_attack_damage = rand.nextInt(5);
+        int mutation_attack_speed = rand.nextInt(5);
+        int mutation_max_health = rand.nextInt(5);
+
+        double mut_scl_pov = (rand.nextInt(11) - 5) / 10.0;
+        double mut_scl_ju_st = (rand.nextInt(11) - 5) / 10.0 * 0.42;
+        double mut_scl_mo_sp = (rand.nextInt(11) - 5) / 10.0 * 0.1;
+        double mut_scl_arm = rand.nextInt(5) - 2;
+        double mut_scl_at_da = rand.nextInt(5) - 2;
+        double mut_scl_at_sp = rand.nextInt(9) - 4;
+        double mut_scl_ma_he = rand.nextInt(11) - 5;
     }
 
     @EventHandler
